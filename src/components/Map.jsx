d530227a53cd2d82eba4e56cdee1dedc25bd756e
codyh587 <108317527+codyh587@users.noreply.github.com>
2024-04-19 20:29:33 -0700

    map component adapts bounds to new coords

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,35 @@
 import { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Polyline, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline, Marker } from 'react-leaflet';
 import { trailinfo } from '../assets/sample_traildata';
 
 export function Map({
-    center = trailinfo.center,
-    polyline = trailinfo.coords,
+    marker = trailinfo.center, // format: [11.00, 11.00]
+    polyline = trailinfo.coords, // format: list[center, center]
 }) {
-    // const map = useMap();
-    useEffect(() => {}, [polyline]);
+    const [bounds, setBounds] = useState(
+        L.polyline(polyline).getBounds().extend(marker)
+    );
 
-    // let pl = L.polyline(
-    //     coordinates
-    // );
+    useEffect(() => {
+        setBounds(L.polyline(polyline).getBounds().extend(marker));
+    }, [polyline]);
 
     return (
         <>
             <MapContainer
                 className="w-full h-full"
-                center={center}
                 zoom={11}
                 scrollWheelZoom={false}
                 preferCanvas={true}
-                // bounds={this.getBounds()}
-                // boundsOptions={{ padding: [50, 50] }}
+                bounds={bounds}
+                boundsOptions={{ padding: [50, 50] }}
             >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <Polyline positions={polyline} />
+                <Marker position={marker} />
             </MapContainer>
         </>
     );
