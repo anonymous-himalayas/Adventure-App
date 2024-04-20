534350ded8c6b8d1c800a9b49a3a10c446e2319f
Himal <hpmalik@uci.edu>
2024-04-20 07:36:26 -0700

    added new gemini prompts for yielding top 5 trails

--- src/utils/getTrailNameGemini.js
+++ src/utils/getTrailNameGemini.js
@@ -45,7 +45,7 @@ const chat = model.startChat({
             role: 'user',
             parts: [
                 {
-                    text: 'You are about to receive information that will pertain to what a user wants in a hiking trail.  You want to give an accurate hiking trail that is best described by the information such as length of trail, terrain difficulty, and facilities onsite. However not all that information may be provided to you and you will give an accurate trail that fits well. You will provide a single hiking trail that is somewhere close to where the user specifies and most accurately fits with the information that the user provided or if no location is given, base it off of a trail that is most accurate to the information provided and closest to the current location of the user. You will only give the trail name and it has to be trail that can be put into Google Maps.',
+                    text: 'You are to receive information that pertains to a specific hiking trail. It is your job to find at least 1 hiking trail with a similar name and if there are many similar names you will choose the top 5 that are most similar. In addition, you are aware that the information given may not actually be a hiking trail and it is your job to realize that and return Invalid Query. Otherwise if the name is similar to at least 1 or more trails then you will list all of them up to 5 trails. In addition, the trails names have to be able to put into Google Maps.',
                 },
             ],
         },
@@ -53,7 +53,7 @@ const chat = model.startChat({
             role: 'model',
             parts: [
                 {
-                    text: 'I am about to receive information that will pertain to what a user wants in a hiking trail. I want to give an accurate hiking trail that is best described by the information such as length of trail, terrain difficulty, and facilities onsite. I realize not all the information may be provided to me and I will give an accurate trail that fits well. I will provide a single hiking trail that is somewhere close to where the user specifies and most accurately fits with the information that the user provided or if no location is given, base it off of a trail that is most accurate to the information provided and closest to the current location of the user. I will only give the trail name that can be put into google maps.',
+                    text: 'I am about to receive information that pertains to a specific hiking trail. It is my job to find at least 1 hiking trail with a similar name and if there are many similar names I will choose the top 5 that are most similar. In addition, you are aware that the information given may not actually be a hiking trail and it is my job to realize that and return Invalid Query. Otherwise if the name is similar to atleast 1 or more trails then I will list all of them up to 5 trails. In addition, the trail names have to be put into Google Maps',
                 },
             ],
         },
@@ -65,3 +65,7 @@ const result = await chat.sendMessage(
 );
 const response = result.response.text();
 console.log(response);
+
+//You are about to receive information that will pertain to what a user wants in a hiking trail.  You want to give an accurate hiking trail that is best described by the information such as length of trail, terrain difficulty, and facilities onsite. However not all that information may be provided to you and you will give an accurate trail that fits well. You will provide a single hiking trail that is somewhere close to where the user specifies and most accurately fits with the information that the user provided or if no location is given, base it off of a trail that is most accurate to the information provided and closest to the current location of the user. You will only give the trail name and it has to be trail that can be put into Google Maps.
+
+//I am about to receive information that will pertain to what a user wants in a hiking trail. I want to give an accurate hiking trail that is best described by the information such as length of trail, terrain difficulty, and facilities onsite. I realize not all the information may be provided to me and I will give an accurate trail that fits well. I will provide a single hiking trail that is somewhere close to where the user specifies and most accurately fits with the information that the user provided or if no location is given, base it off of a trail that is most accurate to the information provided and closest to the current location of the user. I will only give the trail name that can be put into google maps.
