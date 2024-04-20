955de22ec7ac0aca4b581d5e6e8d94d0fb4620a4
Benson Manzano <bensonfmanzano@gmail.com>
2024-04-19 21:57:54 -0700

    added css

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+export function Sidebar() {
+    return (
+        <>
+            <div className="basis-2/12 h-dvh bg-blue-200">
+                <h1>Sidebar</h1>
+            </div>
+        </>
+    );
+}
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import { Map } from '../components/Map';
+import { Navbar } from '../components/Navbar';
+import { Sidebar } from '../components/Sidebar';
 
 export function Home() {
     return (
         <>
             <div className="fixed w-full h-dvh">
-                <Map />
+                <Navbar />
+                <div className="flex flex-row w-full h-dvh align-middle">
+                    <Sidebar />
+                    <div className="basis-10/12 h-dvh" >
+                        <div className=" w-[2] h-[650px] border-2 rounded-3xl m-4" >
+                            <Map/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
