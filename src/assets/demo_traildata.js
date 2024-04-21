cf6362c37d4307fa7612fbeaa4ce523a1234f716
codyh587 <108317527+codyh587@users.noreply.github.com>
2024-04-21 02:31:33 -0700

    changed def. marker

--- src/assets/demo_traildata.js
+++ src/assets/demo_traildata.js
@@ -2,7 +2,7 @@ import trail from './demo_trailcoords';
 
 let coords = [];
 
-const center = [33.8946257, -117.6939622];
+const center = [34.070591, -118.446806];
 
 trail[0]['geojson']['coordinates'].map((coord) => {
     coords.push([coord[1], coord[0]]);
